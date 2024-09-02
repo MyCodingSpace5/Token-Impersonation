f86f6c2cb57be8813b1a64004ab27c2d0c55bbc0
Ahmet Guzel <121121504+MyCodingSpace5@users.noreply.github.com>
2024-09-02 16:00:39 -0500

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -1,16 +1,16 @@
 @echo off
 call :Start
-
+goto :eof
 :Start
-set /p Username = "Enter the Username of the Account you wish to spoof"
-set /p EnterFilePath = "Enter path of file"
+set /p Username="Enter the Username of the Account you wish to spoof: "
+set /p EnterFilePath="Enter path of file: "
+set /p boolean="y/n: "
 echo %username% is correct right?
-set /p boolean = "y/n"
 
-
-if boolean == "y" (
-   powershell -ExecutionPolicy Bypass -File %EnterFilePath% -username %Username%
+if /i "%boolean%"=="y" (
+   powershell -ExecutionPolicy Bypass -File "%EnterFilePath%" -username "%Username%"
 )
 )else(
    call :Start    
 )
+goto :eof
